80b89408b540740146b39dba348fd8dc70f7f820
Severin Buhler <sevi_buehler@hotmail.com>
2024-09-18 10:39:12 +0200

    chore: v1.2.1

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -1,6 +1,6 @@
 import * as secp from "@noble/secp256k1";
 import {utils} from '../src'
-import { bytesToHex, dsha526, hexToBytes } from "../src/utils";
+import { bytesToHex, dsha256, hexToBytes } from "../src/utils";
 
 
 test("Match key string with bytes", async () => {
@@ -21,7 +21,7 @@ test("dsha256", async () => {
         169, 168, 221, 176, 136, 131, 104, 187,
         23, 78, 134, 227, 48, 33, 122, 213
     ]);
-    const hash = dsha526(input)
+    const hash = dsha256(input)
     expect(hash).toEqual(new Uint8Array([179, 34, 87, 183, 183, 156, 156, 56, 23, 65, 122, 250, 131, 252, 176, 48, 203, 212, 161, 7, 140, 69, 140, 99, 185, 22, 74, 197, 114, 194, 250, 132]));
 });
 
